254173fed068c9860b7e5084bfe90b9ef29ea91c
Stephen Suyanka <stepkans@gmail.com>
2024-07-05 11:08:47 -0700

    Reorganised the code block-Added arithmetic module, and a function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,10 +7,11 @@ def perform_operation(num1, num2, operation):
     elif operation == "multiply":
         result = num1 * num2
     elif operation == "divide":
-        if num2 != 0:
-            result = num1/num2
+        if num2 == 0:
+            print("Cannot divide by zero") 
         else:
-            print("Cannot divide by zero")  
+            result = num1 / num2
+             
     else:
         print("Invalid input") 
-    return result             
+    return result     
