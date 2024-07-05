0d7d393d326f103f910a1154b3a97367a6b592a4
Stephen Suyanka <stepkans@gmail.com>
2024-07-05 14:32:14 -0700

    Code Rectification

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,21 +1,32 @@
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
-temp = float(input("Enter the temperature to convert: "))
-temp2 = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
+temperature = float(input("Enter the temperature to convert: "))
+unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
 
-def convert_to_celsius(temp):
-    if temp2 == "F":
-        in_celsius = (temp - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
-        print(f"{temp}°F is {in_celsius}°C")
-
+def convert_to_celsius(temperature):
+    temparature_in_celsius = (temperature - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    return temparature_in_celsius
 
 
-def convert_to_fahrenheit(celsius):
-    if temp2 == "C":
-        in_fahrenheit = (temp * FAHRENHEIT_TO_CELSIUS_FACTOR) + 32
-        print(f"{temp}°C is {in_fahrenheit}°F")
+
+def convert_to_fahrenheit(temperature):   
+    temparature_in_fahrenheit = (temperature * FAHRENHEIT_TO_CELSIUS_FACTOR) + 32
+    return temparature_in_fahrenheit
+
+match unit:
+    case "F":
+        if unit == 'F':
+            converted_temp = convert_to_celsius(temperature)
+            print(f"{temperature}°F is {converted_temp}°C")
+    case "C":
+        if unit == 'C':
+            converted_temp = convert_to_fahrenheit(temperature)
+            print(f"{temperature}°C is {converted_temp}°F")
+
+    case _:
+        raise ValueError("Invalid unit. Please enter 'C' for Celsius or 'F' for Fahrenheit.")        
 
 if __name__ == "__main__":        
-    convert_to_celsius(temp)
-    convert_to_fahrenheit(temp)
+    convert_to_celsius(temperature)
+    convert_to_fahrenheit(temperature)
