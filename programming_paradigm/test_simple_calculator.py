20e20054138bdd36e88883a12104bf5135b3ecf1
Stephen Suyanka <stepkans@gmail.com>
2024-07-13 06:21:29 -0700

    Correct test function methods

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -14,23 +14,28 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(0, 0), 0)
 
     def test_subtraction(self): 
-        """Test the subtract method.""" 
-        self.assertEqual(self.calculator.subtract(5, 2), 3)
-        self.assertEqual(self.calculator.subtract(-5, 2), -7)
-        self.assertEqual(self.calculator.subtract(-2, -2), 0)
-        self.assertEqual(self.calculator.subtract(5, 0), 5)  
+        """Test the subtraction method.""" 
+        self.assertEqual(self.calc.subtract(5, 2), 3)
+        self.assertEqual(self.calc.subtract(-5, 2), -7)
+        self.assertEqual(self.calc.subtract(-2, -2), 0)
+        self.assertEqual(self.calc.subtract(5, 0), 5)  
 
-    def test_multiply(self):  
-        """Test the multiply method.""" 
-        self.assertEqual(self.calculator.multiply(5, 2), 10)
-        self.assertEqual(self.calculator.multiply(-5, 2), -10)
-        self.assertEqual(self.calculator.multiply(-2, -2), 4)     
-        self.assertEqual(self.calculator.multiply(10, 0), 0)     
+    def test_multiplication(self):  
+        """Test the multiplication method.""" 
+        self.assertEqual(self.calc.multiply(5, 2), 10)
+        self.assertEqual(self.calc.multiply(-5, 2), -10)
+        self.assertEqual(self.calc.multiply(-2, -2), 4)     
+        self.assertEqual(self.calc.multiply(10, 0), 0)     
 
-    def test_divide(self):
-        """Test the divide method."""
-        self.assertEqual(self.calculator.divide(5, 2), 2.5)
-        self.assertEqual(self.calculator.divide(-5, 2), -2.5)
-        self.assertEqual(self.calculator.divide(-2, -2), 1)
-        self.assertEqual(self.calculator.divide(0, 1), 0)
-        self.assertIsNone(self.calculator.divide(5, 0))
+    def test_division(self):
+        """Test the division method."""
+        self.assertEqual(self.calc.divide(5, 2), 2.5)
+        self.assertEqual(self.calc.divide(-5, 2), -2.5)
+        self.assertEqual(self.calc.divide(-2, -2), 1)
+        self.assertEqual(self.calc.divide(0, 1), 0)
+        self.assertIsNone(self.calc.divide(5, 0))
+           
+
+
+if __name__ == "__main__":
+    unittest.main()
