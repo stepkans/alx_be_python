375717ffce6971466931bfef33bec5ca3aacff23
Stephen Suyanka <stepkans@gmail.com>
2024-07-13 02:01:22 -0700

    Add better format to display function

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -12,4 +12,4 @@ class BankAccount:
             return True  
         return False
     def display_balance(self):
-        print(f"Current Balance: ${self.__account_balance:.2f}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
