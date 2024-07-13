d5298356a417c1c91119c725a1e16409d084ff16
Stephen Suyanka <stepkans@gmail.com>
2024-07-13 04:09:12 -0700

    Replaced Exception with ValueError

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -4,5 +4,5 @@ def safe_divide(numerator, denominator):
         return f"The result of the division is {result}"
     except ZeroDivisionError:
         return "Error: Cannot divide by zero."   
-    except Exception:
+    except ValueError:
         return "Error: Please enter numeric values only."
